3bb2ab207ddc60827293b851a7f7aac768d6ab06
alexalegor <info@calypto.se>
2016-11-23 14:16:31 +0100

    Adding new NoFooter themes

--- PaysonIntegrationCO2/Models/Enums/GuiTheme.cs
+++ PaysonIntegrationCO2/Models/Enums/GuiTheme.cs
@@ -8,6 +8,9 @@ namespace PaysonIntegrationCO2.Models.Enums
         Gray = 2,
         GrayTextLogos = 3,
         BlueTextLogos = 4,
-        WhiteTextLogos = 5
+        WhiteTextLogos = 5,
+        WhiteNoFooter = 6,
+        GrayNoFooter = 7,
+        BlueNoFooter = 8
     }
 }
